98067eeea987bbe4d44ffebc0234645c2e10389d
Confused Horse <21200630+ConfusedHorse@users.noreply.github.com>
2023-07-14 16:22:40 +0200

    removed unnecessary providers

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,4 @@
-import { importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/component';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    importProvidersFrom(BrowserAnimationsModule)
-  ]
-});
+bootstrapApplication(AppComponent);
